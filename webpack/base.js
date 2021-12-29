77a94c47bac2e2529c1b393a59c173b8ee72e3e4
Zahra Mirali <z.mirali1997@gmail.com>
2021-12-29 21:21:53 +0330

    add rules to webpack base config

--- webpack/base.js
+++ webpack/base.js
@@ -1,1 +1,29 @@
-export default {};
+import path from 'path';
+
+import * as rules from './rules';
+import { isDevServer, isProd } from './utils/env';
+import { arrayFilterEmpty } from './utils/helpers';
+
+export default {
+  context: __dirname,
+  target: isDevServer ? 'web' : ['web', 'es5'],
+  mode: isProd ? 'production' : 'development',
+  output: {
+    publicPath: './',
+    path: path.join(__dirname, '../dist'),
+    filename: isDevServer ? '[name].[fullhash].js' : '[name].[contenthash].js',
+  },
+  module: {
+    rules: arrayFilterEmpty([
+      rules.javascriptRule,
+      rules.typescriptRule,
+      rules.htmlRule,
+      rules.imagesRule,
+      rules.fontsRule,
+      rules.cssRule,
+      ...rules.lessRules,
+      ...rules.sassRules,
+      ...rules.svgRules,
+    ]),
+  },
+};
